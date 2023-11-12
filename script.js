3bda2af6fd49e7afe5c57e85abf2cd75f586a3b1
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2023-11-12 18:59:28 -0500

    Added HTMLelement to Javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+var startButton = document.getElementById('startButton');
+// var quizSection = document.getElementById("quiz");
+// var quizSection = document.getElementById("");
+// var quizSection = document.getElementById("");
+// var quizSection = document.getElementById("");
+// var quizSection = document.getElementById("");
+// var quizSection = document.getElementById("");
